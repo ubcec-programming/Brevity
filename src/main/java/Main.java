71905679b8d9d9bfd1319d06244a99a7a5a646a0
0v0-QAQ <42987966+0v0-QAQ@users.noreply.github.com>
2018-10-21 15:49:54 -0400

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main (String [] arg){
+//        System.out.println("Hellow world!");
+
+    User user1 = new User(new PositionPair(1.0, 1.0), "Ben", 10);
+    user1.newMessage("Hellow!");
+
+    User user2 = new User(new PositionPair(1.0, 2.0), "Harry", 10);
+
+
+
+
+
+    }
+}
