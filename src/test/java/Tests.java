a829eb55c03c46742342b10ec47f91eba4150562
zzp <billzhu564@gmail.com>
2018-10-21 09:19:22 -0700

    Tests changes

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -12,11 +12,10 @@ public class Tests {
     private static User user1 = new User(new PositionPair(1.0, 1.0), "Ben", 10);
     private static User user2 = new User(new PositionPair(1.0, 2.0), "Harry", 10);
 
+
     @BeforeClass
     public static void setup(){
-
         user1.newMessage("Hellow!");
-
 
 
 
