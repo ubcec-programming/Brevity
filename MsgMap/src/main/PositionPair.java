a02ee46d21d734b2c03ca0ec090172a21412523c
evansz12 <sizhe.zhang@ubc.alumni.ca>
2018-10-21 11:03:59 -0700

    start

--- MsgMap/src/main/PositionPair.java
+++ MsgMap/src/main/PositionPair.java
@@ -1,6 +1,20 @@
-public class PositionPair {
+public class PositionPair extends geoMap {
 
     private double latitude, longitude;
 
-    
+        /**
+         * Create a new Location given a latitude and longitude pair
+         *
+         * @param latitude not null
+         * @param longitude not null
+         */
+        public PositionPair(double latitude, double longitude) {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+    }
+
+
+
 }
