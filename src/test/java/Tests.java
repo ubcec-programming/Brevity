cbf3998cd50f09a27bd23d28989b23cf630a2785
zzp <billzhu564@gmail.com>
2018-10-21 09:16:38 -0700

    Tests changes

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -9,19 +9,22 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class Tests {
+    private static User user1 = new User(new PositionPair(1.0, 1.0), "Ben", 10);
+    private static User user2 = new User(new PositionPair(1.0, 2.0), "Harry", 10);
+
     @BeforeClass
     public static void setup(){
-        User user1 = new User(new PositionPair(1.0, 1.0), "Ben", 10);
+
         user1.newMessage("Hellow!");
 
-        User user2 = new User(new PositionPair(1.0, 2.0), "Harry", 10);
+
 
 
     }
 
     @Test
     public void testEqual(){
-        assertEquals(true, true);
+        assertEquals(1, geoMap.getMsgs(user2.getPosition(), 10).size());
     }
 
 
